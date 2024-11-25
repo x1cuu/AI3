097cd6e276b7ace5cc6981e2415bdf5f825bace8
zeto32 <joaoazul1104@gmail.com>
2024-11-25 10:10:46 +0000

    controladores

---
+++ controladores/authController.js
@@ -0,0 +1,12 @@
+exports.register = (req, res) => {
+    res.status(501).json({ message: 'Not implemented yet' });
+  };
+  
+  exports.login = (req, res) => {
+    res.status(501).json({ message: 'Not implemented yet' });
+  };
+  
+  exports.recoverPassword = (req, res) => {
+    res.status(501).json({ message: 'Not implemented yet' });
+  };
+  
---
+++ controladores/reservationController.js
@@ -0,0 +1,20 @@
+exports.create = (req, res) => {
+    res.status(501).json({ message: 'Not implemented yet' });
+  };
+  
+  exports.getAll = (req, res) => {
+    res.status(501).json({ message: 'Not implemented yet' });
+  };
+  
+  exports.getById = (req, res) => {
+    res.status(501).json({ message: 'Not implemented yet' });
+  };
+  
+  exports.update = (req, res) => {
+    res.status(501).json({ message: 'Not implemented yet' });
+  };
+  
+  exports.delete = (req, res) => {
+    res.status(501).json({ message: 'Not implemented yet' });
+  };
+  
