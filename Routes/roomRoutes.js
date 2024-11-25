72c9e526d235e6c3b26c2f7f018ad7c53189e5b1
Reueben <rubenfilipecampos2002@gmail.com>
2024-11-25 11:14:17 +0000

    Fix

--- Routes/roomRoutes.js
+++ Routes/roomRoutes.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
-// Importação do controlador
 const roomController = require('../controllers/roomController');
 
-// Endpoints para gestão de salas
-router.post('/', roomController.create); // Criar uma sala
-
-router.get('/:id', roomController.getById); // Consultar detalhes de uma sala
-router.get('/', roomController.getAll); // Consultar todas as salas
-
-router.put('/:id', roomController.update); // Editar uma sala
-
-router.delete('/:id', roomController.delete); // Remover uma sala
+// Rotas de salas
+router.post('/create', roomController.createRoom); // Criar sala
+router.put('/:id', roomController.updateRoom); // Atualizar sala
+router.delete('/:id', roomController.deleteRoom); // Excluir sala
 
 module.exports = router;
