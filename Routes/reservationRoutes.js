56e31ef8f1e439fdcfd0ce8e0dc8de22c4479bed
Reueben <rubenfilipecampos2002@gmail.com>
2024-11-25 10:59:30 +0000

    Mudanças em routes e controlers

--- Routes/reservationRoutes.js
+++ Routes/reservationRoutes.js
@@ -6,9 +6,12 @@ const reservationController = require('../controllers/reservationController');
 
 // Endpoints para gestão de reservas
 router.post('/', reservationController.create); // Criar uma nova reserva
-router.get('/', reservationController.getAll); // Consultar todas as reservas
+
 router.get('/:id', reservationController.getById); // Consultar uma reserva específica
+router.get('/', reservationController.getAll); // Consultar todas as reservas
+
 router.put('/:id', reservationController.update); // Editar uma reserva
+
 router.delete('/:id', reservationController.delete); // Cancelar uma reserva
 
 module.exports = router;
--- Routes/roomRoutes.js
+++ Routes/roomRoutes.js
@@ -6,9 +6,12 @@ const roomController = require('../controllers/roomController');
 
 // Endpoints para gestão de salas
 router.post('/', roomController.create); // Criar uma sala
-router.get('/', roomController.getAll); // Consultar todas as salas
+
 router.get('/:id', roomController.getById); // Consultar detalhes de uma sala
+router.get('/', roomController.getAll); // Consultar todas as salas
+
 router.put('/:id', roomController.update); // Editar uma sala
+
 router.delete('/:id', roomController.delete); // Remover uma sala
 
 module.exports = router;
